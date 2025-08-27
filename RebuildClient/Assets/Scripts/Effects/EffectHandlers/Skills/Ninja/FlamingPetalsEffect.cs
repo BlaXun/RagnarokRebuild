637d6967022332b37a2eb2efe8dea650d1201466
BlaXun <blaxun01@googlemail.com>
2025-08-27 14:23:26 +0200

    * More changes to the skill. Damage should be correct. HitCount is correct... but getting the visuals right is a pain. Will need to invest more time into this

---
+++ RebuildClient/Assets/Scripts/Effects/EffectHandlers/Skills/Ninja/FlamingPetalsEffect.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Assets.Scripts.Effects.PrimitiveData;
+using Assets.Scripts.Network;
+using Assets.Scripts.Objects;
+using Random = UnityEngine.Random;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Effects.EffectHandlers.General;
+
+namespace Assets.Scripts.Effects.EffectHandlers.Skills
+{
+    [RoEffect("FlamingPetals")]
+    public class FlamingPetalsEffect : IEffectHandler
+    {
+        public static Ragnarok3dEffect Create(ServerControllable source, GameObject target, float delayTime, int level)
+        {
+            var effect = RagnarokEffectPool.Get3dEffect(EffectType.Fireball);
+            effect.SourceEntity = source;
+            effect.AimTarget = target;
+            effect.SetDurationByTime(60);
+            effect.UpdateOnlyOnFrameChange = true;
+            effect.ActiveDelay = delayTime;
+            effect.PositionOffset = target.transform.position;
+            
+            EffectSharedMaterialManager.PrepareEffectSprite("Assets/Sprites/Effects/fireball.spr");
+
+            return effect;
+        }
+        
+        public bool Update(Ragnarok3dEffect effect, float pos, int step)
+        {
+            Debug.Log($"UPDATE");
+            if (!EffectSharedMaterialManager.TryGetEffectSprite("Assets/Sprites/Effects/fireball.spr", out _))
+            {
+                effect.ResetStep();
+                Debug.Log($"true");
+                return true; //wait until the sprite loads
+            }
+            
+            AudioManager.Instance.OneShotSoundEffect(effect.SourceEntity.Id, "ef_fireball.ogg", effect.SourceEntity.transform.position, 0.5f);
+            //RoSpriteProjectileEffect.CreateProjectile(effect.SourceEntity, effect.AimTarget, "Assets/Sprites/Effects/fireball.spr", c, 0);
+            var sc = effect.FollowTarget.GetComponent<ServerControllable>();
+            //RoSpriteProjectileEffect.CreateProjectile(effect.SourceEntity, effect.AimTarget, "Assets/Sprites/Effects/fireball.spr", c, 0);
+            RoSpriteEffect.AttachSprite(effect.SourceEntity, "Assets/Sprites/Effects/msg.spr", 1f, 1f, RoSpriteEffectFlags.EndWithAnimation);
+
+            return step < effect.DurationFrames;
+        }
+        /*public bool Update(Ragnarok3dEffect effect, float pos, int step) {
+
+            if (step % 10 == 0) {
+                Debug.Log($"FollowTarget: {effect.FollowTarget}");
+                var sc = effect.FollowTarget.GetComponent<ServerControllable>();
+                Debug.Log($"sc: {sc}");
+                //RoSpriteEffect.AttachSprite(sc, "Assets/Sprites/Effects/msg.spr", 1f, 1f, RoSpriteEffectFlags.EndWithAnimation);
+            }
+            
+            return effect.IsTimerActive;
+        }*/
+    }
+}
---
+++ RebuildClient/Assets/Scripts/SkillHandlers/Handlers/Ninja/FlamingPetalsHandler.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Effects.EffectHandlers;
+using Assets.Scripts.Effects.EffectHandlers.Skills;
+using Assets.Scripts.Network;
+using JetBrains.Annotations;
+using RebuildSharedData.Enum;
+using RebuildSharedData.Enum.EntityStats;
+using UnityEngine;
+
+using Assets.Scripts.Effects;
+namespace Assets.Scripts.SkillHandlers
+{
+    [SkillHandler(CharacterSkill.FlamingPetals)]
+    public class FlamingPetalsHandler : SkillHandlerBase
+    {
+        public override void OnHitEffect(ServerControllable target, ref AttackResultData attack)  {
+            attack.Target?.Messages.SendHitEffect(attack.Src, attack.DamageTiming, 1, attack.HitCount);
+        }
+        
+        public override void StartSkillCasting(ServerControllable src, ServerControllable target, int lvl, float castTime) {
+            Debug.Log($"StartSkillCasting");
+            HoldStandbyMotionForCast(src, castTime);
+            src.AttachEffect(CastEffect.Create(castTime, src.gameObject, AttackElement.Fire));
+            target?.AttachEffect(CastLockOnEffect.Create(castTime, target.gameObject));
+            Debug.Log($"StartSkillCasting ENDE");
+        }
+
+        public override void ExecuteSkillTargeted([CanBeNull] ServerControllable src, ref AttackResultData attack) {
+            
+            src?.LookAtOrDefault(attack.Target);
+            if (src != null && attack.Target != null && attack.Result != AttackResult.Invisible)  {
+                FlamingPetalsEffect.Create(src, attack.Target.gameObject, 0f, attack.SkillLevel);
+                src?.PerformSkillMotion(true);
+            }
+        }
+    }
+}
