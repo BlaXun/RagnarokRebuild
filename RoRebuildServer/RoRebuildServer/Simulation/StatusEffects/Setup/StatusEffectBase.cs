1e0582d81525bc5610aadc5d94c9c648b81a1156
BlaXun <blaxun01@googlemail.com>
2025-08-29 11:17:00 +0200

    * Added Cicada Skill Handler and Status Effect * Buff Icon isnt working yet * We still have to allow some skills to pass through cicada once we got em * Still missing visual effect when cicada is triggered

--- RoRebuildServer/RoRebuildServer/Simulation/StatusEffects/Setup/StatusEffectBase.cs
+++ RoRebuildServer/RoRebuildServer/Simulation/StatusEffects/Setup/StatusEffectBase.cs
@@ -17,8 +17,17 @@ namespace RoRebuildServer.Simulation.StatusEffects.Setup
 
         public virtual StatusUpdateResult OnUpdateTick(CombatEntity ch, ref StatusEffectState state) => StatusUpdateResult.Continue;
         public virtual StatusUpdateResult OnAttack(CombatEntity ch, ref StatusEffectState state, ref DamageInfo info) => StatusUpdateResult.Continue;
+        
+        /// Occurs when queued damage is applied. Note that this means the client has already has the damage number, so it's not a good place to nullify damage.
+        /// Used to break ice, stone, sleep, and other such statuses on hit.
         public virtual StatusUpdateResult OnTakeDamage(CombatEntity ch, ref StatusEffectState state, ref DamageInfo info) => StatusUpdateResult.Continue;
+        
+        /// A CalculateCombatResult was finished. Lets you modify the results. EnergyCoat uses this to cut damage,
+        /// Hiding uses this to let it ignore non-earth property attacks
         public virtual StatusUpdateResult OnCalculateDamage(CombatEntity ch, ref StatusEffectState state, ref AttackRequest req, ref DamageInfo info) => StatusUpdateResult.Continue;
+        
+        /// A CalculateCombatResult was called, but has not been executed. Used for sleep (guaranteed crit),
+        /// magical attack (replaces atk with matk), and power up (increases skill mod)
         public virtual StatusUpdateResult OnPreCalculateDamage(CombatEntity ch, CombatEntity? target, ref StatusEffectState state, ref AttackRequest req) => StatusUpdateResult.Continue;
         public virtual StatusUpdateResult OnChangeEquipment(CombatEntity ch, ref StatusEffectState state) => StatusUpdateResult.Continue;
         public virtual StatusUpdateResult OnMove(CombatEntity ch, ref StatusEffectState state, Position src, Position dest, bool isTeleport) => StatusUpdateResult.Continue;
