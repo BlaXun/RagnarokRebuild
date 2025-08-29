710ad4aad5c99b7f40560c42d13a3a0b695a7bc1
BlaXun <blaxun01@googlemail.com>
2025-08-29 13:42:23 +0200

    * Added Ninja Aura Skill * Missing the special cast aura... no idea how to add that right now... its only visual anyway

---
+++ RebuildClient/Assets/Scripts/SkillHandlers/Handlers/Ninja/NinjaAuraHandler.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Network;
+using RebuildSharedData.Enum;
+using JetBrains.Annotations;
+using RebuildSharedData.Enum.EntityStats;
+using Assets.Scripts.Effects.EffectHandlers;
+    
+namespace Assets.Scripts.SkillHandlers.Handlers
+{
+    [SkillHandler(CharacterSkill.NinjaAura)]
+    public class NinjaAuraHandler : SkillHandlerBase
+    {
+        public override void StartSkillCasting(ServerControllable src, ServerControllable target, int lvl, float castTime) {
+            HoldStandbyMotionForCast(src, castTime);
+            src.AttachEffect(CastEffect.Create(castTime, src.gameObject, AttackElement.Water));
+        }
+        
+        public override void ExecuteSkillTargeted([CanBeNull] ServerControllable src, ref AttackResultData attack) {
+            src?.PerformSkillMotion();
+        }
+    }
+}
---
+++ RoRebuildServer/RoRebuildServer/Simulation/StatusEffects/ExpandedJobs/StatusNinjaAura.cs
@@ -0,0 +1,35 @@
+﻿using RebuildSharedData.Enum;
+using RebuildSharedData.Enum.EntityStats;
+using RoRebuildServer.EntityComponents.Character;
+using RoRebuildServer.EntityComponents;
+using RoRebuildServer.Simulation.StatusEffects.Setup;
+using RoRebuildServer.EntityComponents.Util;
+using RoRebuildServer.Networking;
+using RoRebuildServer.EntitySystem;
+using RoRebuildServer.Logging;
+
+namespace RoRebuildServer.Simulation.StatusEffects.ExpandedJobs;
+
+[StatusEffectHandler(CharacterStatusEffect.NinjaAura, StatusClientVisibility.Everyone, StatusEffectFlags.NoSave, "NinjaAura")]
+public class StatusNinjaAura : StatusEffectBase
+{
+    public override StatusUpdateMode UpdateMode => StatusUpdateMode.OnUpdate;
+    
+    //Value1: Skill level
+    public override void OnApply(CombatEntity ch, ref StatusEffectState state) {
+        
+        ServerLogger.LogWarning("OnApply");
+        ch.AddStat(CharacterStat.AddStr, state.Value1);
+        ch.AddStat(CharacterStat.AddInt, state.Value1);
+
+        if (ch.Character.State != CharacterState.Dead)
+            ch.Character.State = CharacterState.Idle;
+    }
+
+    public override void OnExpiration(CombatEntity ch, ref StatusEffectState state)  {
+        ServerLogger.LogWarning("OnExpiration");
+        ServerLogger.LogWarning($"{ch}");
+        ch.SubStat(CharacterStat.AddStr, state.Value1);
+        ch.SubStat(CharacterStat.AddInt, state.Value1);
+    }
+}
